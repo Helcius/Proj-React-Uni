6f1f0b56a27d80a6b7478000a4a2ced7a9e823ca
Helcius Cabral <helcius.cabral@hotmail.com>
2024-04-24 20:59:08 -0300

    aula 24.04

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,9 @@
 import "./styles.css";
 
 const Header = () => {
-    return (<div className="header"><h1 className="header_title">React Exercises</h1></div>)
+    return (<div className="header">
+        <h1 className="header_title">React Exercises</h1>
+        </div>)
 }
 
 export default Header;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
+import { SimpleCounter } from "../../components/SimpleCounter";
+
 const Home = () => {
-    return(<p>Home Page</p>)
+    return(<SimpleCounter />)
 }
 
 export default Home;
